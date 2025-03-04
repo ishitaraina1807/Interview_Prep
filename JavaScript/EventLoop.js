a73465acec6a1860c8c32b0334c8cd7e74c907e9
ishitaraina1807 <ishitaraina18@gmail.com>
2025-03-04 19:20:39 +0530

    a setTimeout that will never run

---
+++ JavaScript/EventLoop.js
@@ -0,0 +1,18 @@
+console.log("Start");
+
+// A setTimeout with 0ms delay (macro-task)
+setTimeout(() => {
+  console.log("This will never run!");
+}, 0);
+
+// Function to flood microtasks queue
+function starveEventLoop() {
+  Promise.resolve().then(() => {
+    starveEventLoop();  
+  });
+}
+
+starveEventLoop();
+
+console.log("End");
+
